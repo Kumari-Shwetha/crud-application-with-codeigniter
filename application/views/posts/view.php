43daa824b84c5c18c4fa8c6dbb8c87dc760b412e
Kumari Shwetha <60698897+iamkumarishwetha@users.noreply.github.com>
2020-07-01 11:34:48 +0530

    Update view.php

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -1,15 +1,18 @@
 <div class="container">
-    <div class="row">
-        <div class="panel panel-default">
-            <div class="panel-heading">Post Details <a href="<?php echo site_url('posts/'); ?>" class="fas fa-arrow-left float-right"></a></div>
-            <div class="panel-body">
-                <div class="form-group">
-                    <label>Title:</label>
-                    <p><?php echo !empty($post['title'])?$post['title']:''; ?></p>
-                </div>
-                <div class="form-group">
-                    <label>Content:</label>
-                    <p><?php echo !empty($post['content'])?$post['content']:''; ?></p>
+     <div class="jumbotron mt-2">
+        <div class="row">
+            <div class="panel panel-default">
+                <div class="panel-heading"><span>Post Details</span> <a href="<?php echo site_url('posts/'); ?>" class="btn btn-sm btn-info float-right"><i class="fas fa-arrow-left"></i></a></div>
+                <hr>
+                <div class="panel-body">
+                    <div class="form-group">
+                        <label>Title:</label>
+                        <p><?php echo !empty($post['title'])?$post['title']:''; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Content:</label>
+                        <p><?php echo !empty($post['content'])?$post['content']:''; ?></p>
+                    </div>
                 </div>
             </div>
         </div>
