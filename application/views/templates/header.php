f41905fa7399643ec8ec39abb9f0d388584bfb61
Kumari Shwetha <60698897+iamkumarishwetha@users.noreply.github.com>
2020-07-01 11:35:20 +0530

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,5 +4,31 @@
     <meta charset="utf-8">
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <style>
+    	body{
+    		background-color: #E0E2DB;
+    		color:#fff;
+    	}
+    	.jumbotron{
+    		background-color: #3D348B;
+    		position: absolute;
+    		top: 50%;
+    		left: 50%;
+    		transform: translate(-50%, -50%);
+    		width: 100%;
+    	}
+    	.table{
+    		color:#fff;
+    	}
+    	.container{
+    		position: relative;
+    		min-height: 100vh;
+    	}
+    	.panel-heading span{
+    		font-size: 2rem;
+    		font-weight: 700;
+    		color: #E6AF2E;
+    	}
+    </style>
 </head>
 <body>
